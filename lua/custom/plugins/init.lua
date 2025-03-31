8b3facc86a93d5431ba9ed0bb17f050711615ad0
kudachan <kumarnchandan05@gmail.com>
2025-03-31 23:59:57 +0530

    adding augment

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,9 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  require('lazy').setup {
+    -- Your other plugins here
+    'augmentcode/augment.vim',
+  },
+}
